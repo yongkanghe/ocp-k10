80761d58eac4595802ac5064d6c49ea73a20a348
Yongkang He <he@yongkang.cloud>
2021-12-12 12:11:40 +0800

    Create setenv.sh

---
+++ ocp-ibm-k10/setenv.sh
@@ -0,0 +1,15 @@
+#-------Set the environment variables"
+export MY_CLUSTER=ocp4yong1-ibm  #Customize your favorite cluster name
+export MY_RESOURCE_GROUP=rg4yong1
+export MY_VPC=vpc4yong1
+export MY_SUBNET=subnet4yong1
+export MY_GATEWAY=gateway4yong1
+export MY_OBJECT_STORAGE=cos4yong1
+export MY_SERVICE_KEY=svckey4yong1
+export MY_WORKER_FLAVOR=bx2.8x32 #Customize your favorite worker flavor
+export MY_OCP_VERSION=4.8        #Customize your favorite
+export MY_REGION=au-syd          #Customize your favorite region
+export MY_ZONE=au-syd-3          #Customize your favorite zone
+export MY_BUCKET=k10bucket4yong1 #Customize your favorite bucket
+export MY_OBJECT_STORAGE_PROFILE=myibmcos1 #Customize your favorite profile name
+
