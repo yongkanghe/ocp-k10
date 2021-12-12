9f514121d86f2b6d659feb5d725c714140d8124f
Yongkang He <he@yongkang.cloud>
2021-12-12 11:11:12 +0800

    Update k10-destroy.sh

--- rosa-k10/k10-destroy.sh
+++ rosa-k10/k10-destroy.sh
@@ -1,6 +1,5 @@
 starttime=$(date +%s)
-. setenv.sh
-MY_PREFIX=$(echo $(whoami) | sed -e 's/\_//g' | sed -e 's/\.//g' | awk '{print tolower($0)}')
+. ./setenv.sh
 
 echo "-------Delete mongodb & kasten-io"
 helm uninstall mongodb -n k10-mongodb
@@ -10,9 +9,6 @@ kubectl delete ns kasten-io
 
 echo '-------Deleting objects from the bucket'
 aws s3 rb s3://$(cat rosa_bucketname) --force
-
-# echo '-------Deleting kubeconfig for this cluster'
-# kubectl config delete-context $(kubectl config get-contexts | grep $MY_PREFIX-$MY_CLUSTER | awk '{print $2}')
 
 echo "" | awk '{print $1}'
 endtime=$(date +%s)
