f8a3b0124e7a2235f1aaa3ad5c0da8ae86a0fe8f
Yongkang He <he@yongkang.cloud>
2021-12-12 12:11:20 +0800

    Create deploy.sh

---
+++ ocp-ibm-k10/deploy.sh
@@ -0,0 +1,10 @@
+ocpk10starttime=$(date +%s)
+
+./ocp-deploy.sh
+
+./k10-deploy.sh
+
+ocpk10endtime=$(date +%s)
+ocpk10duration=$(( $ocpk10endtime - $ocpk10starttime ))
+echo "-------Total time is $(($ocpk10duration / 60)) minutes $(($ocpk10duration % 60)) seconds for OCP+OCS+K10+Postgresql+Policy+OnDemandBackup."
+echo "" | awk '{print $1}'
