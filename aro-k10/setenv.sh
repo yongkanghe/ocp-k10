3da3f93ac0f7dcf5bd0e2812b519a136fd6c1c5d
Yongkang He <he@yongkang.cloud>
2022-07-16 10:21:49 +0800

    Update setenv.sh

--- aro-k10/setenv.sh
+++ aro-k10/setenv.sh
@@ -6,6 +6,7 @@ export AZURE_STORAGE_ACCOUNT_ID=azsa4yong1  #Customize your Storage Account
 export MY_REGION="West US"    #Customize region for Blob Storage
 export MY_CONTAINER=k10container4yong1      #Customize your container
 export MY_OBJECT_STORAGE_PROFILE=myazblob1  #Customize your profile name
+export MY_PREFIX=$(echo $(whoami) | sed -e 's/\_//g' | sed -e 's/\.//g' | awk '{print tolower($0)}')
 # export MY_VMSIZE=Standard_D4as_v4   #Customize your VM size
 # export MY_VMSIZE=Standard_D2as_v4   #Customize your VM size
 # export MY_LOCATION=centralindia     #Customize your location
