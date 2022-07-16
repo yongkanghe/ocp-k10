c21218c53eb25f9b29080961f16ad0fc42362292
Yongkang He <he@yongkang.cloud>
2022-07-16 09:36:01 +0800

    Update aroprep.sh

--- aro-k10/aroprep.sh
+++ aro-k10/aroprep.sh
@@ -6,7 +6,7 @@ echo 'export PATH=$PATH:~/ocp-k10/aro-k10' >> ~/.bashrc && source ~/.bashrc
 rm openshift-client-linux.tar.gz
 
 AZURE_SUBSCRIPTION_ID=$(az account list --query "[?isDefault][id]" --all -o tsv)
-az ad sp create-for-rbac --name https://aks4yong1-k10-app --role Contributor --scopes /subscriptions/$AZURE_SUBSCRIPTION_ID -o json > aks4yong1app
+az ad sp create-for-rbac --name https://aks4yong1-k10-app --role Contributor --scopes /subscriptions/$AZURE_SUBSCRIPTION_ID -o json > aro4yong1app
 
 clear
 
