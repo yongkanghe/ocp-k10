c5633be804bfaa5272a71d8105ca78cb65b49aed
Yongkang He <he@yongkang.cloud>
2022-07-23 10:49:25 +0800

    Create aro-az-location.sh

---
+++ aro-k10/aro-az-location.sh
@@ -0,0 +1,31 @@
+. ./setenv.sh
+
+echo '-------Create a Azure Blob Storage profile secret'
+kubectl create secret generic k10-aro-azure-secret \
+      --namespace kasten-io \
+      --from-literal=azure_storage_account_id=$MY_PREFIX$ARO_AZURE_STORAGE_ACCOUNT_ID \
+      --from-literal=azure_storage_key=$(cat aro_az_storage_key)
+
+echo '-------Creating a Azure Blob Storage profile'
+cat <<EOF | kubectl apply -f -
+apiVersion: config.kio.kasten.io/v1alpha1
+kind: Profile
+metadata:
+  name: $ARO_MY_OBJECT_STORAGE_PROFILE
+  namespace: kasten-io
+spec:
+  type: Location
+  locationSpec:
+    credential:
+      secretType: AzStorageAccount
+      secret:
+        apiVersion: v1
+        kind: Secret
+        name: k10-aro-azure-secret
+        namespace: kasten-io
+    type: ObjectStore
+    objectStore:
+      name: $MY_PREFIX-$ARO_MY_CONTAINER
+      objectStoreType: AZ
+      region: $ARO_MY_REGION
+EOF
