07df41f2cfb9a2ef23d56f4e419709729cf7448a
Yongkang He <he@yongkang.cloud>
2022-07-23 09:58:36 +0800

    Update aro-deploy.sh

--- aro-k10/aro-deploy.sh
+++ aro-k10/aro-deploy.sh
@@ -3,23 +3,23 @@ starttime=$(date +%s)
 . ./setenv.sh
 
 az group create \
-  --name $MY_PREFIX-$ARO_MY_GROUP \
+  --name $ARO_MY_PREFIX-$ARO_MY_GROUP \
   --location $ARO_MY_LOCATION
 
 az network vnet create \
-  --resource-group $MY_PREFIX-$ARO_MY_GROUP \
+  --resource-group $ARO_MY_PREFIX-$ARO_MY_GROUP \
   --name aro-vnet4yong1 \
   --address-prefixes 10.8.0.0/23
 
 az network vnet subnet create \
-  --resource-group $MY_PREFIX-$ARO_MY_GROUP \
+  --resource-group $ARO_MY_PREFIX-$ARO_MY_GROUP \
   --vnet-name aro-vnet4yong1 \
   --name master-subnet4yong1 \
   --address-prefixes 10.8.0.0/24 \
   --service-endpoints Microsoft.ContainerRegistry
 
 az network vnet subnet create \
-  --resource-group $MY_PREFIX-$ARO_MY_GROUP \
+  --resource-group $ARO_MY_PREFIX-$ARO_MY_GROUP \
   --vnet-name aro-vnet4yong1 \
   --name worker-subnet4yong1 \
   --address-prefixes 10.8.1.0/24 \
@@ -27,21 +27,21 @@ az network vnet subnet create \
 
 az network vnet subnet update \
   --name master-subnet4yong1 \
-  --resource-group $MY_PREFIX-$ARO_MY_GROUP \
+  --resource-group $ARO_MY_PREFIX-$ARO_MY_GROUP \
   --vnet-name aro-vnet4yong1 \
   --disable-private-link-service-network-policies true
 
 az aro create \
-  --resource-group $MY_PREFIX-$ARO_MY_GROUP \
+  --resource-group $ARO_MY_PREFIX-$ARO_MY_GROUP \
   --name $ARO_MY_CLUSTER \
   --vnet aro-vnet4yong1 \
   --master-subnet master-subnet4yong1 \
   --worker-subnet worker-subnet4yong1 \
   --pull-secret @pull-secret.txt
 
-PASSWORD=$(az aro list-credentials --name $ARO_MY_CLUSTER --resource-group $MY_PREFIX-$ARO_MY_GROUP -o tsv --query kubeadminPassword)
+PASSWORD=$(az aro list-credentials --name $ARO_MY_CLUSTER --resource-group $ARO_MY_PREFIX-$ARO_MY_GROUP -o tsv --query kubeadminPassword)
 
-apiServer=$(az aro show -g $MY_PREFIX-$ARO_MY_GROUP -n $ARO_MY_CLUSTER --query apiserverProfile.url -o tsv)
+apiServer=$(az aro show -g $ARO_MY_PREFIX-$ARO_MY_GROUP -n $ARO_MY_CLUSTER --query apiserverProfile.url -o tsv)
 
 oc login $apiServer -u kubeadmin -p $PASSWORD --insecure-skip-tls-verify
 
