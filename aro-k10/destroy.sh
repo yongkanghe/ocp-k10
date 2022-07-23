9d47d5b6915988896cd5cf2069301fc2f29a5c8f
Yongkang He <he@yongkang.cloud>
2022-07-23 21:21:10 +0800

    Create destroy.sh

---
+++ aro-k10/destroy.sh
@@ -0,0 +1,18 @@
+starttime=$(date +%s)
+. ./setenv.sh
+
+echo '-------Deleting an ARO Cluster (typically in less than 10 mins)'
+az group delete -g $ARO_MY_PREFIX-$ARO_MY_GROUP --yes
+kubectl config delete-context $(kubectl config get-contexts | grep $ARO_MY_CLUSTER | awk '{print $2}')
+echo "" | awk '{print $1}'
+
+# echo '-------Deleting the app registration created by AKS'
+# MYID=$(az ad sp list --show-mine --query [].servicePrincipalNames -o table | grep $MY_PREFIX-$MY_GROUP | awk '{print $2}')
+# az ad app delete --id $MYID
+
+endtime=$(date +%s)
+duration=$(( $endtime - $starttime ))
+echo "-------Total time is $(($duration / 60)) minutes $(($duration % 60)) seconds."
+echo "" | awk '{print $1}'
+echo "-------Created by Yongkang"
+echo "-------Email me if any suggestions or issues he@yongkang.cloud"
