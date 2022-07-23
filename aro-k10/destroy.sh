d6da229438c55acd6d1667ae9d01449354098c00
Yongkang He <he@yongkang.cloud>
2022-07-23 21:48:53 +0800

    Update destroy.sh

--- aro-k10/destroy.sh
+++ aro-k10/destroy.sh
@@ -4,7 +4,7 @@ starttime=$(date +%s)
 echo '-------Deleting an ARO Cluster (typically in less than 10 mins)'
 az group delete -g $ARO_MY_PREFIX-$ARO_MY_GROUP --yes
 
-ARO_CONTEXT=$(az aro list | grep aro4yong1 | awk '{print $6}' | sed -e 's/https\:\/\/console-openshift-console\.//' | sed -e 's/\///' | sed -e 's/apps\.//' | sed -e 's/\.westus\.aroapp\.io//')
+ARO_CONTEXT=$(az aro list | grep $ARO_MY_CLUSTER | awk '{print $6}' | sed -e 's/https\:\/\/console-openshift-console\.//' | sed -e 's/\///' | sed -e 's/apps\.//' | sed -e 's/\.westus\.aroapp\.io//')
 kubectl config delete-context $(kubectl config get-contexts | grep $ARO_CONTEXT | awk '{print $2}')
 echo "" | awk '{print $1}'
 
