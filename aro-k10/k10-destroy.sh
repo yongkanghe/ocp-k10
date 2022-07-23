0955b3a55b6a74d81e16c01776616d837f651893
Yongkang He <he@yongkang.cloud>
2022-07-23 21:11:06 +0800

    Update k10-destroy.sh

--- aro-k10/k10-destroy.sh
+++ aro-k10/k10-destroy.sh
@@ -8,6 +8,9 @@ helm uninstall k10 -n kasten-io
 kubectl delete ns yong-postgresql
 kubectl delete ns kasten-io
 
+echo '-------Deleting objects from Azure Blob Storage Bucket'
+az storage blob delete-batch --account-name $ARO_MY_PREFIX$ARO_AZURE_STORAGE_ACCOUNT_ID -s $ARO_MY_PREFIX-$ARO_MY_CONTAINER --account-key $(cat aro_az_storage_key)
+
 echo "" | awk '{print $1}'
 endtime=$(date +%s)
 duration=$(( $endtime - $starttime ))
