01e1a58a8f7f9d77a3925fbe8e77c60693ddc254
Yongkang He <he@yongkang.cloud>
2022-07-24 18:15:55 +0800

    Update aroprep.sh

--- aro-k10/aroprep.sh
+++ aro-k10/aroprep.sh
@@ -1,8 +1,8 @@
 echo "-------Install oc client, create an app registration"
 cd ~/ocp-k10/aro-k10
 wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz
-tar -zxvf openshift-client-linux.tar.gz
-echo 'export PATH=$PATH:~/ocp-k10/aro-k10' >> ~/.bashrc && source ~/.bashrc
+tar -zxvf openshift-client-linux.tar.gz oc
+# echo 'export PATH=$PATH:~/ocp-k10/aro-k10' >> ~/.bashrc && source ~/.bashrc
 rm openshift-client-linux.tar.gz
 
 ./createapp.sh
