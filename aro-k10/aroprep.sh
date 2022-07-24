97a9e9a8f19694669871f84da3ff220f796f388e
Yongkang He <he@yongkang.cloud>
2022-07-24 18:29:07 +0800

    Update aroprep.sh

--- aro-k10/aroprep.sh
+++ aro-k10/aroprep.sh
@@ -2,6 +2,7 @@ echo "-------Install oc client, create an app registration"
 cd ~/ocp-k10/aro-k10
 wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz
 tar -zxvf openshift-client-linux.tar.gz oc
+ln -s oc kubectl
 
 cat ~/.bashrc | grep aro-k10
 if [ `echo $?` -eq 1 ]
@@ -13,9 +14,9 @@ rm openshift-client-linux.tar.gz
 
 ./createapp.sh
 
-echo -n "Log into Red Hat Cloud Console https://cloud.redhat.com"
-echo -n "Click OpenShift, Click Downloads, Scroll down to the bottom"
-echo -n "Click Copy button under Tokens section, Paste your pull secret and press [ENTER]: "
+echo -n "\nLog into Red Hat Cloud Console https://cloud.redhat.com"
+echo -n "\nClick OpenShift, Click Downloads, Scroll down to the bottom"
+echo -n "\nClick Copy button under Tokens section, Paste your pull secret and press [ENTER]: "
 read pullsecret
 echo "" | awk '{print $1}'
 echo $pullsecret > pull-secret.txt
