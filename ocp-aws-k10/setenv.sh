5532077c86be65b77f888bd76993eb3f4c6745a7
Yongkang He <he@yongkang.cloud>
2022-08-03 11:27:16 +0800

    Create setenv.sh

---
+++ ocp-aws-k10/setenv.sh
@@ -0,0 +1,4 @@
+#-------Set the environment variables"
+export OCP_AWS_MY_REGION=us-east-2          #Customize your favorite region
+export OCP_AWS_MY_BUCKET=ocp-aws-k10bucket4yong1    #Customize your favorite bucket
+export OCP_AWS_MY_OBJECT_STORAGE_PROFILE=ocp-aws-myaws3 #Customize your favorite profile name
