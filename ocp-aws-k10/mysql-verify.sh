88fd65b8e82f6271547ec8fb62013d3fc1237b91
Yongkang He <he@yongkang.cloud>
2022-09-18 13:58:23 +0800

    Update mysql-verify.sh

--- ocp-aws-k10/mysql-verify.sh
+++ ocp-aws-k10/mysql-verify.sh
@@ -1,5 +1,5 @@
 #Verify if the data has been recoved in the target cluster
-oc exec -ti mysql-0 -n yong-mysql -c mysql -- bash
-mysql --user=root --password=password4yong1
+oc exec -ti mysql-0 -n yong-mysql -c mysql -- mysql --user=root --password=password4yong1<<EOF
 USE db4yong1;
 SELECT * FROM employee;
+EOF
