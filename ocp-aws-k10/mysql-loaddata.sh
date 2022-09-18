51ebc652739ff64f369cb9becda1451268c33ba4
Yongkang He <he@yongkang.cloud>
2022-09-18 13:58:21 +0800

    Update mysql-loaddata.sh

--- ocp-aws-k10/mysql-loaddata.sh
+++ ocp-aws-k10/mysql-loaddata.sh
@@ -1,8 +1,8 @@
-#Create a DB, Table and insert data from the source cluster
-oc exec -ti mysql-0 -n yong-mysql -c mysql -- bash
-mysql --user=root --password=password4yong1
+#Create a DB, Table and insert data
+oc exec -ti mysql-0 -n yong-mysql -c mysql -- mysql --user=root --password=password4yong1<<EOF
 CREATE DATABASE db4yong1;
 USE db4yong1;
 CREATE TABLE employee (emp_name VARCHAR(20), emp_id int(5), location VARCHAR(20));
 INSERT INTO employee VALUES ('Yongkang He','1','Singapore');
 SELECT * FROM employee;
+EOF
