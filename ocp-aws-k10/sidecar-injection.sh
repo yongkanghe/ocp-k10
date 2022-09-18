0e179a2bd3d1911677bb232ccf7fcf8fc7999774
Yongkang He <he@yongkang.cloud>
2022-09-18 14:34:26 +0800

    Create sidecar-injection.sh

---
+++ ocp-aws-k10/sidecar-injection.sh
@@ -0,0 +1,4 @@
+#Inject the sidecar to enable the Generic Volume Snapshot backup
+wget https://github.com/kastenhq/external-tools/releases/download/4.0.2/k10tools_4.0.2_linux_amd64
+chmod +x k10tools_4.0.2_linux_amd64
+./k10tools_4.0.2_linux_amd64 k10genericbackup inject all -n yong-mysql  
