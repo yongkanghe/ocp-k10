61e29ba3f1781e89a4df2782169759cb5813ab84
Yongkang He <he@yongkang.cloud>
2022-09-18 14:39:31 +0800

    Update sidecar-injection.sh

--- ocp-aws-k10/sidecar-injection.sh
+++ ocp-aws-k10/sidecar-injection.sh
@@ -1,4 +1,4 @@
-#Inject the sidecar to enable the Generic Volume Snapshot backup
+#Inject the sidecar to enable the Generic Volume Snapshot backup, only required for old system without CSI drivers
 wget https://github.com/kastenhq/external-tools/releases/download/4.0.2/k10tools_4.0.2_linux_amd64
 chmod +x k10tools_4.0.2_linux_amd64
 ./k10tools_4.0.2_linux_amd64 k10genericbackup inject all -n yong-mysql  
