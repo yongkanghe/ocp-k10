453aee2d1c38ca7422df9707850d643f0674c610
Yongkang He <he@yongkang.cloud>
2022-09-18 13:39:27 +0800

    Create mysql-loaddata.sh

---
+++ ocp-aws-k10/mysql-loaddata.sh
@@ -0,0 +1,8 @@
+#Create a DB, Table and insert data
+oc exec -ti mysql-0 -n yong-mysql -c mysql -- bash
+mysql --user=root --password=password4yong1
+CREATE DATABASE db4yong1;
+USE db4yong1;
+CREATE TABLE employee (emp_name VARCHAR(20), emp_id int(5), location VARCHAR(20));
+INSERT INTO employee VALUES ('Yongkang He','1','Singapore');
+SELECT * FROM employee;
